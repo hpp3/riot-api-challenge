3d109b133b2677506e3c76088ded73874d5664c4
Eddy Gao <ewgao@uwaterloo.ca>
2015-08-16 04:52:12 -0400

    include timeline data

--- download.py
+++ download.py
@@ -44,11 +44,11 @@ for f in [f for f in os.listdir(input_directory) if path.isfile(path.join(input_
         filename = path.join(output_directory, region, str(game) + '.json')
         if not path.isfile(filename):
             print "Downloading %s (%d of %d)" % (filename, i+1, len(games))
-            r = requests.get(apiBaseUrl[region.lower()] + match_api % (region.lower(), game), params={'api_key': api_key, 'include_timeline': True})
+            r = requests.get(apiBaseUrl[region.lower()] + match_api % (region.lower(), game), params={'api_key': api_key, 'includeTimeline': 'true'})
             while r.status_code == 429:
                 print "Retrying in 10 seconds"
                 sleep(10)
-                r = requests.get(apiBaseUrl[region.lower()] + match_api % (region.lower(), game), params={'api_key': api_key, 'include_timeline': True})
+                r = requests.get(apiBaseUrl[region.lower()] + match_api % (region.lower(), game), params={'api_key': api_key, 'includeTimeline': 'true'})
             outfile = open('temp', 'w')
             outfile.write(r.text.encode('utf-8'))
             outfile.close()
