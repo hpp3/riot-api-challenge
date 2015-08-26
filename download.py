0e16637108e14a8c311cc70eb3da5c8b07f54650
Eddy Gao <ewgao@uwaterloo.ca>
2015-08-25 21:36:17 -0400

    updated download script

--- download.py
+++ download.py
@@ -10,7 +10,7 @@ from time import sleep
 api_keys = [open('eddy.apikey').read().strip(),
             open('jason.apikey').read().strip()]
 api_key = api_keys[0] 
-input_directory = path.join('AP_ITEM_DATASET','5.11','RANKED_SOLO')
+input_directory = path.join('AP_ITEM_DATASET','5.14','RANKED_SOLO')
 output = 'output'
 output_directory = path.join(input_directory, output) 
 
@@ -37,23 +37,23 @@ if not path.exists(output_directory):
     print "creating output directory %s" % output_directory
     os.mkdir(output_directory)
 
-def process(api_key, region, name=-1):
+def process(api_key, region, name='N/A'):
     global games
     while (games):
         game = games.pop()
         size = len(games)
         filename = path.join(output_directory, region, str(game) + '.json')
         if not path.isfile(filename):
-            print "[Thread %d] Downloading %s (%d left)" % (name, filename, size)
+            print "[Thread %s] Downloading %s (%d left)" % (name, filename, size)
             ok = False
             while not ok:
                 r = requests.get(apiBaseUrl[region.lower()] + match_api % (region.lower(), game), params={'api_key': api_key, 'includeTimeline': 'true'})
                 if r.status_code == 429:
-                    print "Retrying in 10 seconds"
+                    print "[Thread %s] Retrying in 10 seconds" % name
                     sleep(10)
                 elif r.status_code != 200:
-                    print 'received bad status code %s for game %s, retrying in a minute' % (game, r.status_code)
-                    sleep(60)
+                    print '[Thread %s] received bad status code %s for game %s, retrying in 10s (request was %s, %s)' % (name, r.status_code, game, apiBaseUrl[region.lower()] + match_api % (region.lower(), game), {'api_key': api_key, 'includeTimeline': 'true'})
+                    sleep(10)
                     #raise RuntimeError('returned bad status code %s' % r.status_code)
                 else:
                     ok = True
@@ -67,7 +67,7 @@ def process(api_key, region, name=-1):
                 if path.isfile(tempname): os.remove(tempname)
                 if path.isfile(filename): os.remove(filename)
                 raise e
-            sleep(1.5)
+            sleep(1.4)
 
 for f in [f for f in os.listdir(input_directory) if path.isfile(path.join(input_directory, f))]:
     region = f[:-5]
@@ -77,15 +77,11 @@ for f in [f for f in os.listdir(input_directory) if path.isfile(path.join(input_
         print "creating output directory %s" % path.join(output_directory, region)
     games = set([game for game in json.load(open(path.join(input_directory,f))) if not path.isfile(path.join(output_directory, region, str(game) + '.json'))])
     threads = []
-    q = 0
     for apikey in api_keys:
-        threads.append(threading.Thread(target=process, args=[apikey, region, q]))
-        q += 1
+        threads.append(threading.Thread(target=process, args=[apikey, region, ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(3))]))
     for thread in threads:
         thread.daemon = True
         thread.start()
-    while threading.active_count():
-        pass
-    # while all([thread.is_alive() for thread in threads]):
-    #     pass
+    while all([thread.is_alive() for thread in threads]):
+        sleep(10)
 
